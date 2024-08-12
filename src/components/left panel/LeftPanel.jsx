782b7deed1a085683433c3b4e65bb37575906240
TejasChalke <tejaschalke66@gmail.com>
2024-08-12 17:17:00 +0530

    add form to create new rooms

--- src/components/left panel/LeftPanel.jsx
+++ src/components/left panel/LeftPanel.jsx
@@ -1,17 +1,74 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import './LeftPanel.scss'
 import UserContext from '../../contexts/UserContext'
 import { SelectedChannelContext } from '../../contexts/SelectedChannelContext';
 
 export default function LeftPanel() {
     const { user } = useContext(UserContext);
-    const { selectedChannel } = useContext(SelectedChannelContext);
+    const { selectedChannel, setSelectedChannel } = useContext(SelectedChannelContext);
+
+    const [showForm, setShowForm] = useState(false);
+    const [roomName, setRoomName] = useState("");
+    const [newRoomType, setNewRoomType] = useState("text");
 
     const textChannels = selectedChannel.rooms.filter((channel) => channel.type === "text");
     const voiceChannels = selectedChannel.rooms.filter((channel) => channel.type === "voice");
 
     var tag = "" + user.tag;
     while(tag.length < 4) tag = "0" + tag;
+
+    function closeForm(type) {
+        if(type === "add") {
+            setRoomName("");
+            setShowForm(false);
+        }
+    }
+
+    function displayForm(type) {
+        setNewRoomType(type);
+        setShowForm(true);
+    }
+
+    async function addNewRoom() {
+        if(roomName.trim().length < 4) {
+            console.log("Enter a valid room name");
+            return;
+        }
+
+        const body = {
+            "channelId": selectedChannel.id,
+            "name": roomName.trim(),
+            "type": newRoomType
+        }
+
+        try {
+            const response = await fetch("http://localhost:8080/api/room/", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            });
+
+            if(response.status === 201) {
+                console.log("Room created successfully");
+                closeForm("add");
+
+                const channelDataResponse = await fetch(`http://localhost:8080/api/channel/${selectedChannel.id}`);
+                if(channelDataResponse.status === 500) {
+                    console.log("Error getting channel data from server");
+                    return;
+                }
+
+                const channelData = await channelDataResponse.json();
+                setSelectedChannel({...channelData, id: selectedChannel.id, name: selectedChannel.name});
+            } else {
+                console.log("Error creating room");
+            }
+        } catch (error) {
+            console.log("Error making api request to add new room: ", error);
+        }
+    }
 
     return(
         <div id="leftPanelContainer">
@@ -24,7 +81,7 @@ export default function LeftPanel() {
                 <div className="leftPanelList">
                     <div className="leftPanelListHeader">
                         <span>Text Channels</span>
-                        <i className="fa-solid fa-plus"></i>
+                        <i className="fa-solid fa-plus" onClick={() => displayForm("text")}></i>
                     </div>
                     <div className="leftPanelListChannels">
                         {
@@ -43,7 +100,7 @@ export default function LeftPanel() {
                 <div className="leftPanelList">
                     <div className="leftPanelListHeader">
                         <span>Voice Channels</span>
-                        <i className="fa-solid fa-plus"></i>
+                        <i className="fa-solid fa-plus" onClick={() => displayForm("voice")}></i>
                     </div>
                     <div className="leftPanelListChannels">
                         {
@@ -95,6 +152,34 @@ export default function LeftPanel() {
                         <i className="fa-solid fa-volume-high"></i>
                         {/* <i className="fa-solid fa-volume-xmark"></i> */}
                         <i className="fa-solid fa-gear"></i>
+                    </div>
+                </div>
+            </div>
+
+            <div id="popUpForm" className={showForm ? "active" : ""}>
+                <div id="popUpFormHeader">
+                    <div className="big">Create a new room</div>
+                </div>
+                <div className="popUpInput">
+                    <label htmlFor="channel-name">ROOM NAME</label>
+                    <input type="text" name='channel-name' value={roomName} onChange={(e) => setRoomName(e.target.value)}/>
+                </div>
+                <div className="popUpInput buttons">
+                    <div
+                        className="button"
+                        tabIndex={0}
+                        onClick={() => closeForm("add")}
+                        onKeyDown={(e) => {e.key === "Enter" && closeForm("add")}}
+                    >
+                        Back
+                    </div>
+                    <div
+                        className="button background"
+                        tabIndex={0}
+                        onClick={addNewRoom}
+                        onKeyDown={(e) => {e.key === "Enter" && addNewRoom()}}
+                    >
+                        Create
                     </div>
                 </div>
             </div>
